7836f2787e744cec9eb44110eda385c7798d9bd8
ighour <ti.celio.rodrigues@gmail.com>
2020-11-02 15:24:07 +0000

    Fixed fonts.

--- src/assets/jss/material-kit-react/popoverStyles.js
+++ src/assets/jss/material-kit-react/popoverStyles.js
@@ -9,7 +9,7 @@ const popoverStyles = {
     borderRadius: "3px",
     display: "block",
     maxWidth: "276px",
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: '"Frutiger LT Std 45 Light", "Roboto", "Helvetica", "Arial", sans-serif',
     fontStyle: "normal",
     fontWeight: "400",
     textAlign: "start",
